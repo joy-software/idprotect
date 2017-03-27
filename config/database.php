14eb8a92dc0a484ca097ed4e7e631f9e32266422
Joy_Admin <joyjedid@gmail.com>
2017-03-27 16:37:12 +0100

    third commit

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'eu-cdbr-west-01.cleardb.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_3eb9fd73f431797'),
+            'username' => env('DB_USERNAME', 'b3ca824de0752f'),
+            'password' => env('DB_PASSWORD', '9c9d823d'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
