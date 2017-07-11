36cffcfab37d5af0e35459544770e69110d89527
joy-software <joyjedid@gmail.com>
2017-07-11 23:53:17 +0100

    Update SearchController.php

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -242,7 +242,7 @@ class SearchController extends Controller
         if($request->hasFile('avatar')){
             $avatar = $request->file('avatar');
             $filename = time() . '.' . $avatar->getClientOriginalExtension();
-            Image::make($avatar)->resize(300, 300)->save( public_path('/uploads/avatars/' . $filename ) );
+            Image::make($avatar)->save( public_path('/uploads/avatars/' . $filename ) );
 
             $user = Auth::user();
             $user->avatar = $filename;
